5d869183baa6c88985c14b6a1a6ac0ae796760dc
chiheb-devops <166850971+chiheb-devops@users.noreply.github.com>
2024-06-01 12:52:12 +0100

    Delete Backend-services/admin/service-categorie directory

--- Backend-services/admin/service-categorie/config/database.js
+++
@@ -1,8 +0,0 @@
-  const mysql = require("mysql");
-  const connection = mysql.createConnection({
-      host: "mydb",
-      database: "management",
-      user: "root",
-      password: "VdZDPFXDxpedA123",
-    });
-    module.exports = connection
