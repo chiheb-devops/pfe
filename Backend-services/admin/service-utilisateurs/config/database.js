dbd830a2325cde293a1ec4359453e1b0dae7974a
chiheb-devops <166850971+chiheb-devops@users.noreply.github.com>
2024-06-03 21:52:45 +0100

    Update database.js

--- Backend-services/admin/service-utilisateurs/config/database.js
+++ Backend-services/admin/service-utilisateurs/config/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "mydb",
-  database: "management",
-  user: "root",
-  password: "VdZDPFXDxpedA123",
-});
-module.exports = connection
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+  module.exports = connection
 
   /**
    * 
